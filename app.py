483e249b019ed441add72262f14bad0f25d1e556
Brian J King <brianjking@users.noreply.github.com>
2023-03-27 11:54:59 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pandas as pd
+import openpyxl
+import torch
+import pathlib
+impport pil
+import salesforce-lavis  
+from io import BytesIO
+from pathlib import Path
+from PIL import Image
+from lavis.models import load_model_and_preprocess
+from lavis.processors import load_processor
+
+model, preprocess = load_model_and_preprocess()  # Assuming this function loads the desired model
+processor = load_processor()  # Assuming this function loads the desired processor
+
+
+# Define function to run image classification tests using lavis library
+def run_tests(image_files, selected_tests):
+    # Implement the actual image classification logic using lavis and torch
+    # For demonstration purposes, we'll create a simple results DataFrame
+    results = pd.DataFrame({'Image': ['image1.jpg', 'image2.jpg'],
+                            'Test1': ['Pass', 'Fail'],
+                            'Test2': ['Fail', 'Pass']})
+    return results
+
+# Define function to generate Excel file from DataFrame
+def generate_excel_file(df):
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
+        df.to_excel(writer, index=False)
+    return output.getvalue()
+
+# Application interface
+st.title('Image Classification Application')
+
+# File uploader for images
+uploaded_files = st.file_uploader('Upload Images', type=['jpg', 'png'], accept_multiple_files=True)
+
+# Multiselect for test options
+test_options = ['Test1', 'Test2', 'Test3']
+selected_tests = st.multiselect('Select Tests to Run', test_options)
+
+# Run tests button
+if st.button('Run Tests'):
+    # Check if files and tests are selected
+    if uploaded_files and selected_tests:
+        # Run tests and get results as DataFrame
+        test_results = run_tests(uploaded_files, selected_tests)
+
+        # Display test results in tabular format
+        st.dataframe(test_results)
+
+        # Generate Excel file and provide download link
+        excel_file = generate_excel_file(test_results)
+        st.download_button(label='Download Excel File', data=excel_file, file_name='test_results.xlsx')
+    else:
+        st.warning('Please upload images and select tests to run.')
+
